72b1227bdcaff7d3b45255bc285d245b2b38d1a3
wonder0128 <110625211+wonder0128@users.noreply.github.com>
2023-02-14 22:01:35 +0900

    first

---
+++ hello.py
@@ -0,0 +1,112 @@
+# print("Hello world!!")
+
+# a = 1.23
+# print(a)
+# print(type(a))
+
+# 숫자형 예제
+# a = 3
+# b = 4
+
+# print(a + b)
+# print(a - b)
+# print(a * b)
+# print(a / b)
+# print(a % b)
+# print(a ** b)
+# print(a // b)
+
+# 문자열 예제
+# a = "Hello world!!"
+# a = 'Python\'s favorite food is perl'
+# b = """Life is too short
+# You need pyton"""
+
+# c = "Python "
+# d = "is Fun"
+# print(c * 2)
+
+# e = "Life is too short You need Python"
+# print(e[5])
+# print(e[0:4])
+# print(e[8:])
+# print(e[::2])
+
+
+# number = 10
+# day = "three"
+# f = "I eat %dapples. so I was sick for %s days." %(number, day)
+# print(f)
+
+# g = "123asd {age} dfg123 {name} dadgda".format(age=10, name="탄산수")
+# print(g)
+
+# name = "int"
+# h = f"나의 이름은 {name}입니다."
+# print(h)
+
+# i = "%10s" % "hi"
+# print(i)
+
+# j = "%0.4f" %1.234567890
+# print(j)
+
+# a = "Life is too short"
+# print(a.split())
+
+# a = ["나", "너", "우리", "너희", "모두"]
+# print(a[1])
+
+# a = ["나", "너", "우리", "너희", ["모두","My",1 ,2]]
+# print(a[4][1])
+# a[4] = "모두"
+# print(a)
+
+# a = ['나', '너', '우리', '너희', '모두']
+# a[0:2] = ["짱아", "단이"]
+# print(a)
+
+# a = ['나', '너', '우리', '너희', '모두']
+# a[0:2] = []
+# print(a)
+
+# a = [1, 2, 3]
+# a.append("안녕")
+# print(a) 
+
+# a = ['하', '가', '차', '안녕']
+# a.extend([1, 2])
+# print(a)
+
+# 튜플은 값 변경 불가
+t1 = (1, 2, 'a', 'b')
+# del t1[0] 
+# t1.appned[0]
+# t1[0] = 3
+# print(t1[0])
+# print(t1[0:2])
+
+# 딕셔너리
+dic = {'name':'roze', 'age':'30'}
+# print(dic['name'])
+
+# a['name'] = "익명"
+# del a[1]
+
+# print(a.keys())
+# print(a.values())
+# print(a.items())
+# a.clear()
+
+# a = {1:'roze', 2:"짱아", 3:'단이'}
+# print(1 in a)
+
+# 집합
+s1 = set("hello")
+# ㄴ1 = {1,2,3}
+print(s1)
+
+s1 = set([1,2,3,4,5,6])
+s1.remove(1)
+print(s1)
+
